4fb6741dc3c88201495dc025b4902a66632838ba
zereight <fullstackmachinelearning@gmail.com>
2021-01-25 00:03:46 +0900

    feat: 초기 블록 상태 셋팅

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const boardElement = document.querySelector(".board");
 
+const randomPoint = () => {
+    let boxNumber = Math.floor(Math.random() * 16);
+    if(Array.from(boardElement.childNodes).every(cell=>cell.innerHTML!=='0')){
+        // All cell filled
+        console.log('all cells filled');
+        return;
+    }
+    while(Array.from(boardElement.childNodes)[boxNumber].innerHTML !== '0'){
+        boxNumber = Math.floor(Math.random() * 16);
+    }
+
+    Array.from(boardElement.childNodes)[boxNumber].innerHTML = '2';
+}
+
 const init = () => {
   for (let i = 0; i < 16; i++) {
     const newCell = document.createElement("div");
@@ -7,7 +21,9 @@ const init = () => {
     newCell.innerHTML = "0";
     boardElement.appendChild(newCell);
   }
-  
+
+  randomPoint();
+  randomPoint();
 };
 
 init();
